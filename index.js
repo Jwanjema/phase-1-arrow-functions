078401ba970e5a6e4bf4a1764933e2cbc2aed34f
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-09 13:03:22 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function divide ( ) {
+
+}
+
+let a = 2000
+let b = 100
+function divide(a, b) {
+  return a / b;
+}
+
+const square = (x) => {
+  return x * x;
+}
+
+const add = (a, b) => {
+  return a + b;
+}
+
+module.exports = {
+  divide,
+  square,
+  add
+};
